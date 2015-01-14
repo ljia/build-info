340cb9e7a8f823a8d3bdeb7316e4093fc9f68104
Jia, Lin <ljia@verisign.com>
2015-01-14 16:34:13 -0500

    details -> allDeployDetails.

--- build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/task/BuildInfoBaseTask.java
+++ build-info-extractor-gradle/src/main/groovy/org/jfrog/gradle/plugin/artifactory/task/BuildInfoBaseTask.java
@@ -418,7 +418,7 @@ public abstract class BuildInfoBaseTask extends DefaultTask {
          */
         List<DeployDetails> duplicateArtifacts = new ArrayList<DeployDetails>();
         boolean foundDuplicate = false;
-        for (GradleDeployDetails detail : details) {
+        for (GradleDeployDetails detail : allDeployDetails) {
             DeployDetails deployDetails = detail.getDeployDetails();
             String artifactPath = deployDetails.getArtifactPath();
             if (PatternMatcher.pathConflicts(artifactPath, patterns)) {
